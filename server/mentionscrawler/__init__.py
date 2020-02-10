b0261431f8e80bac84dc0c3b0d1de709432af60a
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-02-10 18:44:57 -0500

    Base __init__.py created to be the base of the app, have basic user model, will test adding a user in a moment

--- server/mentionscrawler/__init__.py
+++ server/mentionscrawler/__init__.py
@@ -1,13 +1,14 @@
-import os
-
 from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     if test_config is None:
-        app.config.from_pyfile('config.py'), silent=True
+        app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
 
+    from mentionscrawler.models import db
+    db.init_app(app)
+    
     return app
---
+++ server/mentionscrawler/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return "id: {}, email: {}".format(self.id, self.email)
