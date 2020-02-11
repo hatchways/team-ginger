532f614c0246a39d5a6d98a5842c79864c06daa7
Ryan Narine <ryannarine97@gmail.com>
2020-02-11 16:42:09 -0500

    Implemented Logic -added input validation (email, required inputs, and at least 6 characters in password) -added error messages to the UI -added request and response logic
    Going to do the sign in page now

---
+++ client/src/Constants.js
@@ -0,0 +1,10 @@
+/* Store constant values here */
+
+export const minPasswordLength = 6;
+export const minPasswordErrMsg = `Your password must be at least ${minPasswordLength} characters long`;
+export const takenEmailErrMsg = "That email is already taken";
+
+const serverUrl = "http://localhost:5000/";
+const clientUrl = "http://localhost:3000/";
+export const serverSignUpUrl = serverUrl + "users";
+export const clientDashboardUrl = clientUrl + "dashboard";
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import Input from "@material-ui/core/Input";
+import TextField from "@material-ui/core/TextField";
 import Box from "@material-ui/core/Box";
-import FormControl from "@material-ui/core/FormControl";
 import Button from "@material-ui/core/Button";
+import { minPasswordLength, minPasswordErrMsg, serverSignUpUrl, clientDashboardUrl, takenEmailErrMsg } from "../Constants";
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
     signup_form_container: {
         margin: "auto",
         width: "fit-content",
@@ -25,15 +25,14 @@ const useStyles = makeStyles(theme => ({
     signup_form__inputs: {
         marginTop: theme.spacing(1),
         marginBottom: theme.spacing(1),
-        marginLeft: theme.spacing(6),
-        marginRight: theme.spacing(6),
+
         padding: theme.spacing(2),
-        // Give the inputs that 'pill' outline
-        borderRadius: 500,
-        width: 300
+        width: 300,
+        display: "block"
     },
 
     signup_form__btn: {
+        display: "block",
         backgroundColor: theme.primary,
         color: theme.secondary,
         borderRadius: 500,
@@ -47,45 +46,114 @@ const useStyles = makeStyles(theme => ({
             color: theme.primary
         }
     }
-}));
+});
 
-function SignupForm() {
-    const classes = useStyles();
+class SignupForm extends Component {
+    constructor() {
+        super();
+        this.email = React.createRef();
+        this.name = React.createRef();
+        this.password = React.createRef();
+        // Using state to keep track of errors and respond appropriately
+        this.state = { emailErr: false, passwordErr: false, emailErrMsg: "", passwordErrMsg: "" };
+    }
 
-    return (
-        <Box className={classes.signup_form_container} borderRadius={8} boxShadow={1}>
-            <FormControl className={classes.signup_form} autoComplete="off">
-                <Typography variant="h5" align="center">
-                    <Box fontWeight="fontWeightBold">Let's get Started!</Box>
-                </Typography>
+    handleSubmit = e => {
+        e.preventDefault();
 
-                <Typography variant="subtitle1" align="center" className={classes.signup_form__subtitle}>
-                    Create an account
-                </Typography>
+        let email = this.email.current.value;
+        let name = this.name.current.value;
+        let password = this.password.current.value;
 
-                <Box border={1} className={classes.signup_form__inputs}>
-                    <Input placeholder="Your email" type="email" inputProps={{ "aria-label": "email" }} disableUnderline />
-                </Box>
+        if (this.validate(email, password)) {
+            fetch(serverSignUpUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email, name, password })
+            })
+                .then(res => res.json())
+                .then(data => {
+                    // Token received => success
+                    console.log(data);
+                    if (data["token"]) {
+                        localStorage.setItem("token", data["token"]);
+                        window.location = clientDashboardUrl;
+                    }
+                    // Assume failure means the email is taken
+                    this.setState({ emailErr: true, emailErrMsg: takenEmailErrMsg, passwordErr: false, passwordErrMsg: "" });
+                })
+                .catch(err => console.error("Error: ", err));
+        }
+    };
 
-                <Box border={1} className={classes.signup_form__inputs}>
-                    <Input placeholder="Your company name" inputProps={{ "aria-label": "company name" }} disableUnderline />
-                </Box>
+    // Helper function to check for incorrect inputs (short password, invalid character, etc)
+    validate = (email, password) => {
+        let valid = true;
+        if (password.length < minPasswordLength) {
+            this.setState({ passwordErr: true, passwordErrMsg: minPasswordErrMsg });
+            valid = false;
+        }
+        return valid;
+    };
 
-                <Box border={1} className={classes.signup_form__inputs}>
-                    <Input
+    render() {
+        const { classes } = this.props;
+        return (
+            <Box className={classes.signup_form_container} borderRadius={8} boxShadow={1}>
+                <form className={classes.signup_form} onSubmit={this.handleSubmit}>
+                    <Typography variant="h5" align="center">
+                        <Box fontWeight="fontWeightBold">Let's get Started!</Box>
+                    </Typography>
+
+                    <Typography variant="subtitle1" align="center" className={classes.signup_form__subtitle}>
+                        Create an account
+                    </Typography>
+
+                    <TextField
+                        className={classes.signup_form__inputs}
+                        variant="outlined"
+                        fullWidth
+                        placeholder="Your email"
+                        type="email"
+                        inputProps={{ "aria-label": "email" }}
+                        required
+                        inputRef={this.email}
+                        error={this.state.emailErr}
+                        helperText={this.state.emailErrMsg}
+                    />
+
+                    <TextField
+                        className={classes.signup_form__inputs}
+                        variant="outlined"
+                        fullWidth
+                        placeholder="Your company name"
+                        inputProps={{ "aria-label": "company name" }}
+                        required
+                        inputRef={this.name}
+                    />
+
+                    <TextField
+                        className={classes.signup_form__inputs}
+                        variant="outlined"
+                        fullWidth
                         placeholder="Your password"
                         type="password"
                         inputProps={{ "aria-label": "password" }}
-                        disableUnderline
+                        required
+                        inputRef={this.password}
+                        error={this.state.passwordErr}
+                        helperText={this.state.passwordErrMsg}
                     />
-                </Box>
 
-                <Button variant="contained" className={classes.signup_form__btn} align="center">
-                    Create
-                </Button>
-            </FormControl>
-        </Box>
-    );
+                    <Button variant="contained" type="submit" className={classes.signup_form__btn} align="center">
+                        Create
+                    </Button>
+                </form>
+            </Box>
+        );
+    }
 }
 
-export default SignupForm;
+export default withStyles(styles)(SignupForm);
