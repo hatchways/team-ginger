2e6fd01d399d062f1b2d3ae95c7f3569da969b8b
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-02-17 13:37:22 -0500

    Changed companies route to use insert_rows instead of insert row, that way it adds all the rows to the session, then does one commit

--- server/mentionscrawler/blueprints/company.py
+++ server/mentionscrawler/blueprints/company.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from ..models.company import Company
-from ..db import insert_row
+from ..db import insert_rows
 from ..responses import ok_response, bad_request_response
 from ..authentication.authenticate import authenticate, enforce_json
 
@@ -16,7 +16,9 @@ def update_companies(user):
     if len(names) == 0:
         return bad_request_response("Must have at least one company name")
     Company.query.filter_by(mention_user_id = user.get("user_id")).delete()
+    companies = []
     for name in names:
-        insert_row(Company(user.get("user_id"), name))
+        companies.append(Company(user.get("user_id"), name))
+    insert_rows(companies)
     return ok_response("Company names updated!")
 
