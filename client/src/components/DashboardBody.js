a16afccec48df223f92af1dcacec2330ee162a61
Ryan Narine <ryannarine97@gmail.com>
2020-02-27 14:00:01 -0500

    Fixed Dev
    I don't know exactly what happened but my revert messed up things and not all of nested dialog features made it during the last merge. But now I made the necessary fixes.

--- client/src/components/DashboardBody.js
+++ client/src/components/DashboardBody.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Tab from "@material-ui/core/Tab";
-import Mention from "./Mention";
 import { MENTIONS_ROUTE } from "../Routes";
 import Reddit from "../assets/reddit.png";
 import { RESPONSE_TAG, COMPANY_NAMES_TAG } from "../Constants";
+import Mention from "./Mention";
+import Dialog from "./Dialog";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const LOADING_MESSAGE = "Loading Mentions";
 // Map the name of a site to their logo image reference
 const SITE_TO_IMG = { Reddit };
-
 // Max character limit of mention title and snippet
 const MAX_TITLE_CHARACTERS = 100;
 const MAX_SNIPPET_CHARACTERS = 280;
-
 const styles = theme => ({
     container: {
         width: "90%",
@@ -59,7 +59,6 @@ const styles = theme => ({
         height: "70vh"
     }
 });
-
 class DashboardBody extends Component {
     constructor(props) {
         super(props);
@@ -70,10 +69,8 @@ class DashboardBody extends Component {
             hasMore: true
         };
     }
-
     fetchMentions = () => {
         const { page, mentions } = this.state;
-
         fetch(MENTIONS_ROUTE + "/" + page, { method: "GET", headers: { "Content-Type": "application/json" } }).then(res => {
             if (res.status === 204) {
                 // no more mentions to fetch
@@ -84,7 +81,7 @@ class DashboardBody extends Component {
                         // concatenate the new mentions
                         let newMentions = mentions;
                         let numEntries = Object.entries(newMentions).length;
-                        data.forEach((mention, index) => (newMentions[numEntries++] = mention));
+                        data.forEach(mention => (newMentions[numEntries++] = mention));
                         this.setState({ mentions: newMentions, page: page + 1 });
                     } else {
                         console.log(res.status, data[RESPONSE_TAG]);
@@ -93,7 +90,6 @@ class DashboardBody extends Component {
             }
         });
     };
-
     normalizeSnippet = (snippet, regex) => {
         if (snippet.length < MAX_SNIPPET_CHARACTERS) {
             return snippet;
@@ -102,7 +98,6 @@ class DashboardBody extends Component {
         if (match) {
             // Index of first match
             const index = match.index;
-
             // Index is in the first MSC characters
             if (index < MAX_SNIPPET_CHARACTERS) {
                 return snippet.substring(0, MAX_SNIPPET_CHARACTERS);
@@ -120,9 +115,7 @@ class DashboardBody extends Component {
             return snippet.substring(0, MAX_SNIPPET_CHARACTERS);
         }
     };
-
     normalizeTitle = title => (title > MAX_TITLE_CHARACTERS ? title.substring(0, MAX_TITLE_CHARACTERS) + "..." : title);
-
     render() {
         const { classes } = this.props;
         const { tabValue, mentions, hasMore } = this.state;
@@ -132,13 +125,13 @@ class DashboardBody extends Component {
         reg = reg.join("|");
         // g = global flag, i = ignorecase flag
         const regex = new RegExp(reg, "i");
+        const globalRegex = new RegExp(reg, "gi");
 
         const renderMentions = [];
         if (Object.entries(mentions).length !== 0) {
             Object.entries(mentions).forEach(([key, mention]) => {
                 // trim long snippets and titles
                 let snippet = this.normalizeSnippet(mention.snippet, regex);
-
                 let title = this.normalizeTitle(mention.title);
                 renderMentions.push(
                     <Mention
@@ -154,7 +147,6 @@ class DashboardBody extends Component {
                 );
             });
         }
-
         return (
             <div className={classes.container}>
                 <div className={classes.top_section}>
@@ -193,13 +185,16 @@ class DashboardBody extends Component {
                     {renderMentions}
                     {renderMentions.length !== 0 ? <hr></hr> : ""}
                 </InfiniteScroll>
+
+                <Route
+                    path={`/dashboard/mention/:id`}
+                    component={props => <Dialog id={props.match.params.id} regex={globalRegex} history={props.history} />}
+                />
             </div>
         );
     }
-
     componentDidMount() {
         this.fetchMentions();
     }
 }
-
 export default withStyles(styles)(DashboardBody);
