ecc46f8c6736fc1683dbad5cb736cab4b0fc42bd
Ryan Narine <ryannarine97@gmail.com>
2020-03-02 00:26:07 -0500

    Made a weekly email template You can now send the weekly report email with some test data in defined send.py. Once again I commented out the lines to send the email so you can test it out

--- server/email/send.py
+++ server/email/send.py
@@ -4,6 +4,24 @@ SENDGRID_API_KEY = "SG.BALqFmRPT7mQhwhVKNQCag.WFAEutQpqODtJc5kcEUfO7RmG3TpudnCj1
 FROM_EMAIL = "ryannarine97@gmail.com"
 WELCOME_SUBJECT = "Welcome to mentionscrawler"
 WELCOME_TEMPLATE_ID = "d-335f9dca0ced402aabcc72e3a352c265"
+WEEKLY_TEMPLATE_ID = "d-2f796ef4ab8541dbb30dc80d62a1fd86"
+TEST_DATA_1 = {
+    "warn": True,
+    "month": "Oct",
+    "dayStart": 11,
+    "dayEnd": 17,
+    "mentions": [{"title": "Paypal invested $500 million into company ABC", "source": "Reddit", "snippet": "Man Paypal made a huge mistake" 
+        }, {"title": "Company ABC flees with Paypal investment", "source": "Facebook", "snippet": "Everyone but Paypal saw this coming"}]    
+}
+
+TEST_DATA_2 = {
+    "warn": True,
+    "empty": True,
+    "month": "Nov",
+    "dayStart": 9,
+    "dayEnd": 16,
+    "mentions": []    
+}
 
 def welcome_email(email, company):
     message = {
@@ -24,6 +42,28 @@ def welcome_email(email, company):
             'email': FROM_EMAIL
         },
         'template_id': WELCOME_TEMPLATE_ID
+        ,
+    }
+    sg = SendGridAPIClient(SENDGRID_API_KEY)
+    sg.send(message)
+
+def weekly_email(email,  data = TEST_DATA_1, no_crawlers = False):
+    message = {
+        'personalizations': [
+            {
+                'to': [
+                    {
+                        'email': email
+                    }
+                ],
+                'subject': 'Your Weekly Mentions',
+                'dynamic_template_data': TEST_DATA_1
+            }
+        ],
+        'from': {
+            'email': FROM_EMAIL
+        },
+        'template_id': WEEKLY_TEMPLATE_ID
         ,
     }
     sg = SendGridAPIClient(SENDGRID_API_KEY)
--- server/mentions_crawler_flask/blueprints/user.py
+++ server/mentions_crawler_flask/blueprints/user.py
@@ -7,7 +7,7 @@ from ..models.company import Company
 from ..models.site import get_sites
 from ..db import insert_row, commit
 from ..responses import bad_request_response, ok_response, created_response
-from ...email.send import welcome_email
+from ...email.send import welcome_email, weekly_email
 
 user_bp = Blueprint("users", __name__, url_prefix="/")
 
@@ -32,6 +32,8 @@ def add():
 
     # Send welcome email
     # welcome_email(new_user.email, new_company.name)
+    # Send weekly email
+    # weekly_email(new_user.email)
 
     return created_response("Account successfully created!", new_user.email, [new_company.name],
                             new_user.id, get_sites())
